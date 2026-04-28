945b52466398f2d359e1a631fe0c22361fadbb8f
Mathan495 <mathansk2710@gmail.com>
2026-04-28 11:48:12 +0530

    update settings.py

--- system_settings/settings.py
+++ system_settings/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-=35io=m4#*bfu$%50_v5uow-l*dypemsktl5_%aj5ig)c3nm2q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
